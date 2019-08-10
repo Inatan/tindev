81bf9e38ecb166534e5e6f10f7ba0d9f1d02a6d9
Inatan Lopes Hertzog <inatan.hertzog@conectt.com.br>
2019-08-10 12:04:46 -0300

    frontend finished

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,14 +2,19 @@ import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
-
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png'
+
+import io from 'socket.io-client';
+
 import api from '../services/api'
+
 import './Main.css'
 
 
 export default function Main({ match }){
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect( () => {
         async function loadUsers() {
@@ -20,6 +25,16 @@ export default function Main({ match }){
         }
         loadUsers();
 
+    }, [match.params.id])
+
+    useEffect( () => {
+        const socket = io('http://localhost:3333', {
+           query: {user: match.params.id}
+        });
+        
+        socket.on('match', dev =>{
+            setMatchDev(dev);
+        });
     }, [match.params.id])
 
     async function handleLike(id){
@@ -54,10 +69,10 @@ export default function Main({ match }){
                             <p> {user.bio} </p>
                         </footer>
                         <div className="buttons">
-                            <button type="button" onClick={() => handleDislike(user._id)}>
+                            <button type="button"  onClick={() =>  handleLike(user._id)}>
                                 <img src={like} alt="Like"/>
                             </button>
-                            <button type="button"  onClick={() =>  handleLike(user.y_id)}>
+                            <button type="button" onClick={() => handleDislike(user._id)}>
                                 <img src={dislike} alt="Dislike"/>
                             </button>
                         </div>
@@ -69,6 +84,17 @@ export default function Main({ match }){
                 <div className="empty">Acabou :(</div>
             )
             }
+             {
+                 matchDev && ( 
+                    <div className="match-container">
+                        <img src={itsamatch} alt="It's a match"/>
+                        <img className="avatar" src={matchDev.avatar} alt=""/>
+                        <strong>{matchDev.name}</strong>
+                        <p> {matchDev.bio} </p>
+                        <button type="button" onClick={ () => setMatchDev(null)} > Fechar</button>
+                    </div>
+                )
+             }
         </div>
     );
 }
